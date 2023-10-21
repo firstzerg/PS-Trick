a66e58fa5fb74f0fed18dd0ee7b091f08f3a3a92
hronoas <chronoas@mail.ru>
2023-10-21 14:22:26 +0300

    Update PS-Trick.cmd

--- PS-Trick.cmd
+++ PS-Trick.cmd
@@ -1,30 +1,42 @@
 <# :
-    @echo off 
-    set args=%*
-if not defined args goto noargs
-    set args=%args:"=`"""%
-:noargs
-    powershell /nologo /noprofile /command ^
-    "&{[ScriptBlock]::Create( '$thisfilename="""%~f0"""' + [Char]10 + ((cat """%~f0""") -join [Char]10) ).Invoke("""%args%""")}"
+  @echo off
+  setlocal
+  set "fn=%~f0"
+  set "fn=%fn:'=''%"
+  echo %* | powershell /nologo /noprofile /command "$arguments=$($input | out-string).trim(); &{[ScriptBlock]::Create( (get-content -literalpath '%fn%') -join [Char]10).Invoke('%fn%',$arguments)}"
+  endlocal
   exit /b
 #>
+param($thisfilename,$arguments="")
 
 $arch = @{$true="x86";$false="x64"}[$env:Processor_Architecture -eq "x86"]
-$thisfile=$thisfilename
-$thisdir=split-path $thisfile
+$thisfile = if ($thisfilename) { $thisfilename } else { $MyInvocation.MyCommand.Definition }
+$thisbase = (Get-Item $thisfile).BaseName
+$thisdir = Split-Path $thisfile
 $datetime = Get-Date -format yyyy-MM-dd_HH-mm-ss
 $caption = split-path $thisfile -leaf
 
-function Elevate(){
+$params = @{}
+if ($arguments) {
+  $arguments | Select-String -Pattern '\s*(?<main>.*?)[\s]*(?<=\s|^)\/(?<key>\w+)(?:\s+|$)(?<value>.*?)?(?:\s+|$)?(?=\s*$|(?<=\s|^)\/\w)' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object {
+    if ($_.Groups["main"].Value) { $params.main = $params.main + ($_.Groups["main"].Value -replace '^(["]?)(.*?)\1*$','$2') }
+    $params.($_.Groups["key"].Value) = if ($_.Groups["value"].Value) { ($_.Groups["value"].Value -replace '\\/','/') -replace '^(["]?)(.*?)\1*$','$2' } else { $true }
+  }
+}
+if ($params.Count -eq 0) {
+  $params.main = ($arguments -replace '^(["]?)(.*?)\1*$','$2')
+}
+
+function Elevate{
 	$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 	$myWindowsPrincipal = new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
 	$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 	if ($myWindowsPrincipal.IsInRole($adminRole)){
 		$Host.UI.RawUI.WindowTitle = $thisfile + " (Elevated)"
-		clear-host
 	}else{
+		$cdir=(Get-Location).Path
 		$newProcess = new-object System.Diagnostics.ProcessStartInfo "cmd.exe";
-		$newProcess.Arguments = "/C `"$thisfilename`"";
+		$newProcess.Arguments = "/C `"cd /D `"$cdir`" && `"$thisfilename`" $arguments`"";
 		$newProcess.Verb = "runas";
 		[System.Diagnostics.Process]::Start($newProcess);
 		exit
